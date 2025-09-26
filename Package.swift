e4b35ed25d14d8a541b262e82a16cd21d3ae867b
The iOS Team @ GeneXus <builder@genexus.com>
2025-09-26 07:01:20 -0400

    Package version: 3.2.0

--- Package.swift
+++ Package.swift
@@ -1,28 +1,28 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
 	name: "GXCoreUI",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("15.0"), .watchOS("10.0"), .tvOS("18.0"), .visionOS("2.0")],
 	products: [
 		.library(
 			name: "GXCoreUI",
 			targets: ["GXCoreUIWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "3.2.0")
 	],
 	targets: [
 		.target(name: "GXCoreUIWrapper",
 				dependencies: [
 					"GXCoreUI",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .tvOS, .watchOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .watchOS, .tvOS, .visionOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreUI",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreUI-1.1.0.xcframework.zip",
-			checksum: "3bef10ff01b8a1af978a9f87505f9b06ede005f1bbd0aabf3a5bc969d702793e"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreUI-3.2.0.xcframework.zip",
+			checksum: "9ee0d5bfd9cc02597d05b7b6caecdeed73d960a3acae4a3836ade120a675d1ae"
 		)
 	]
 )
