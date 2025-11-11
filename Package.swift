cd58c4cde2d59726fb71e2a0096e0c46d8c991a2
The iOS Team @ GeneXus <builder@genexus.com>
2025-11-11 09:05:03 -0500

    Package version: 4.1.0-beta.6

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreUIWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "4.1.0-beta.5")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "4.1.0-beta.6")
 	],
 	targets: [
 		.target(name: "GXCoreUIWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreUI",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreUI-4.1.0-beta.5.xcframework.zip",
-			checksum: "e717d5c727fce92bc83b814543f4ec78ac8ce0cdedb330772a06f61b62cbbd06"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreUI-4.1.0-beta.6.xcframework.zip",
+			checksum: "43baa241dd0be70777df10be3b42bd02b875f176f3bbf45543cc1175c1229dce"
 		)
 	]
 )
